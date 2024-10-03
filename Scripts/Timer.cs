e4251b86ec1df037f8cbfac8ee83c438c9085bf4
MajestyCorp <101559700+MajestyCorp@users.noreply.github.com>
2024-10-03 14:19:20 +0200

    Create Timer.cs

---
+++ Scripts/Timer.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class Timer
+{
+    public bool IsActive { get { return Time.time < timeEnd; } }
+    public bool IsFinished { get { return Time.time >= timeEnd; } }
+    public float Progress { get { return CalcProgress(); } }
+    private float timeEnd = 0f, timeStart = 0f;
+
+
+    public void Activate(float timeAmount)
+    {
+        timeStart = Time.time;
+        timeEnd = timeStart + timeAmount;
+    }
+
+    public void Activate(float timeAmount, float progress = 0f)
+    {
+        timeStart = Time.time - timeAmount * progress;
+        timeEnd = timeStart + timeAmount;
+    }
+
+    private float CalcProgress()
+    {
+        if (IsFinished)
+            return 1f;
+        else
+            return (Time.time - timeStart) / (timeEnd - timeStart);
+    }
+}
